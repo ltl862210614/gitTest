e8fbf908f01eeb6731f4bcd4064ef330f78a9c21
liutl <862210614@qq.com>
2021-07-07 10:09:30 +0800

    [NewFeature]新建gitTest仓库，提交测试文件
    Signed-off-by: liutl <862210614@qq.com>

---
+++ test.c
@@ -0,0 +1,23 @@
+/****************************************************************************
+* @File:		test.c
+* @Copyright:	Copyright (C) 2021 by Liutl. All rights reserved.
+* @Author：		Rain
+* @Date:        2021-07-07
+* @Version：	V1.0.0
+* @Description：
+* @Revisions:	
+*  Date           Version	Author       Notes
+*  2021-07-07     1.0.0		Rain       	 the first version
+****************************************************************************/
+#include "stdio.h"
+
+int main(void)
+{
+	printf("this is an eg for git\n");
+
+	while(1)
+	{
+	}
+
+	return 0;
+}
